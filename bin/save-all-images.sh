e4ad1c37e16e21e40f9371794f9647ce5164b80d
weijia.liu <weijia.liu@sky-data.cn>
2017-12-07 15:25:02 +0800

    update bin/save-all-images.sh and add nginx ingress conf files

--- bin/save-all-images.sh
+++ bin/save-all-images.sh
@@ -21,9 +21,13 @@ CALICO_NODE_VERSION=v2.6.3
 CALICO_KUBECTL_VERSION=v1.0.1
 CALICO_CNI_VERSION=v1.11.1
 COREOS_ETCD_VERSION=v3.1.10
+DEFAULTBACKEND_VERSION=1.4
+NGINX_INGRESS_CONTROLLER_VERSION=0.9.0
+
 GCR_URL=gcr.io/google_containers
 CALICO_URL=quay.io/calico
 COREOS_URL=quay.io/coreos
+INGRESS_URL=quay.io/kubernetes-ingress-controller
 
 kube_images=(kube-apiserver-amd64:${KUBE_VERSION}
 kube-controller-manager-amd64:${KUBE_VERSION}
@@ -34,13 +38,16 @@ k8s-dns-kube-dns-amd64:${KUBE_DNS_VERSION}
 k8s-dns-dnsmasq-nanny-amd64:${KUBE_DNS_VERSION}
 etcd-amd64:${KUBE_ETCD_VERSION}
 pause-amd64:${KUBE_PAUSE_VERSION}
-kubernetes-dashboard-amd64:${KUBE_DASHBOARD_VERSION})
+kubernetes-dashboard-amd64:${KUBE_DASHBOARD_VERSION}
+defaultbackend:${DEFAULTBACKEND_VERSION})
 
 calico_images=(node:${CALICO_NODE_VERSION}
 kube-controllers:${CALICO_KUBECTL_VERSION}
 cni:${CALICO_CNI_VERSION})
 
 coreos_images=(etcd:${COREOS_ETCD_VERSION})
+
+ingress_images=(nginx-ingress-controller:${NGINX_INGRESS_CONTROLLER_VERSION})
 
 for image in ${kube_images[@]} ; do
     echo "begin to save ${image} ..."
@@ -63,3 +70,9 @@ for image in ${coreos_images[@]} ; do
     echo "save ${image} finished"
 done
 
+for image in ${ingress_images[@]} ; do
+    echo "begin to save ${image} ..."
+    name=$(echo ${image} | awk -F":" '{print $1}')
+    docker save -o ${SAVE_DIR}/${name}.tar ${INGRESS_URL}/${image}
+    echo "save ${image} finished"
+done
